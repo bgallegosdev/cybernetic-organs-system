e759b87021b4bceff3a4e52e2a34a3cbf2722ab1
bgallegosdev <148573680+bgallegosdev@users.noreply.github.com>
2024-09-21 21:31:38 -0500

    Initial Assignment 4

--- src/main/java/com/cybernetic/Main.java
+++ src/main/java/com/cybernetic/Main.java
@@ -5,15 +5,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Create Organs Objects for Recursion Simulation
-        Heart h = new Heart();
-        Lung l = new Lung();
-        Brain b = new Brain();
 
-        //Create Cybernetic Organ Object for Recursion Simulation
-        CyberneticOrgan trifecta = new CyberneticOrgan();
-
-        //Call to the Recursive Method, startSimulation
-        trifecta.startSimulation(100, h, l, b);
     }
 }
