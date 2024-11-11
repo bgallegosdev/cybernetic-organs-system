3dfc6da5272a0a36ae4c652fbe68a83307cf7640
bgallegosdev <148573680+bgallegosdev@users.noreply.github.com>
2024-11-11 17:54:09 -0600

    final-part-1 edit 4 on 11/11/24

--- src/main/java/com/cybernetic/CyberneticOrgan.java
+++ src/main/java/com/cybernetic/CyberneticOrgan.java
@@ -17,5 +17,90 @@ public class CyberneticOrgan
     private String status; //Valid Values: (AVAILABLE, ALLOCATED, DEFECTIVE)
     private String manufacturer;
 
+    /*
+     * Getters for CyberneticOrgan Class
+     */
+    public String getId() {
+        return id;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+    public double getCompatibilityScore() {
+        return compatibilityScore;
+    }
+    public LocalDate getManufactureDate() {
+        return manufacturerDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    /*
+     * Setters for CyberneticOrgan Class
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+    public void setCompatibilityScore(double compatibilityScore) {
+        this.compatibilityScore = compatibilityScore;
+    }
+    public void setManufactureDate(LocalDate manufacturerDate) {
+        this.manufacturerDate = manufacturerDate;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    /*
+     * Constructor for CyberneticOrgan Class with arguments
+     */
+    public CyberneticOrgan(String id, String type, String model, int powerLevel, double compatibility, LocalDate manufacturerDate, String status, String manufacturer) {
+        this.id = id;
+        this.type = type;
+        this.model = model;
+        this.powerLevel = powerLevel;
+        this.compatibilityScore = compatibility;
+        this.manufacturerDate = manufacturerDate;
+        this.status = status;
+        this.manufacturer = manufacturer;
+    }
+
+    /*
+     * Constructor for CyberneticOrgan Class without arguments
+     */
+    public CyberneticOrgan() {
+        this.id = "";
+        this.type = "";
+        this.model = "";
+        this.powerLevel = 0;
+        this.compatibilityScore = 0.0;
+        this.manufacturerDate = LocalDate.now();
+        this.status = "";
+        this.manufacturer = "";
+    }
+
+
 
 }
