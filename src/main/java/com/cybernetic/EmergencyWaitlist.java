cb335fee26faa694c2c1fa1b0b0569c6d3b9ac45
bgallegosdev <148573680+bgallegosdev@users.noreply.github.com>
2024-11-19 17:19:07 -0600

    final-part-2 initial edit 5 on 11/19/24

--- src/main/java/com/cybernetic/EmergencyWaitlist.java
+++ src/main/java/com/cybernetic/EmergencyWaitlist.java
@@ -8,6 +8,18 @@ import java.util.Queue;
 public class EmergencyWaitlist implements Comparator<EmergencyCase> {
     //Instantiate priority queue
     Queue<EmergencyCase> emergencyCases = new PriorityQueue<>(this);
+    private EmergencyCase root; //root of the EmergencyWaitlist
+
+    //Constructor for EmergencyWaitlist class
+    public EmergencyWaitlist() {
+        this.emergencyCases = new PriorityQueue<>((c1, c2) -> {
+            //compare by severity level, then by registration time
+            if (c1.getSeverityLevel() != c2.getSeverityLevel()) {
+                return Integer.compare(c2.getSeverityLevel(), c1.getSeverityLevel());
+            }
+            return c1.getRegistrationTime().compareTo(c2.getRegistrationTime());
+        });
+    }
 
     /**
      * Method addEmergencyCase to add a new EmergencyCase to the EmergencyWaitlist
@@ -24,7 +36,7 @@ public class EmergencyWaitlist implements Comparator<EmergencyCase> {
      * @return case is the next EmergencyCase in the EmergencyWaitlist
      */
     public EmergencyCase getNextUrgentCase() {
-        return emergencyCases.peek();
+        return emergencyCases.poll();
     }
 
     /**
@@ -41,6 +53,36 @@ public class EmergencyWaitlist implements Comparator<EmergencyCase> {
         }
     }
 
+    /**
+     * Method findCaseById to find a specific EmergencyCase by its caseId using Binary Search Tree
+     * @param caseId case to find in the tree
+     * @return the EmergencyCase found by caseId
+     */
+    public EmergencyCase findCaseById(String caseId) {
+        return findCaseByIdRec(root, caseId);
+    }
+    //helper method for findCaseById to relate data to Nodes with the EmergencyCase class
+    private EmergencyCase findCaseByIdRec(EmergencyCase root, String caseId) {
+        if (root == null || root.getCaseId().equals(caseId)) {
+            return root;
+        }
+        if (caseId.compareTo(root.getCaseId()) < 0) {
+            return findCaseByIdRec(root.getLeft(), caseId);
+        }
+        return findCaseByIdRec(root.getRight(), caseId);
+    }
+
+    /**
+     * Method getAllCaseBySeverity to get all EmergencyCases by severity level
+     * @param severityLevel severity level to find in the tree
+     * @return the EmergencyCases found by severity level
+     */
+    public EmergencyCase[] getAllCaseBySeverity(int severityLevel) {
+        return emergencyCases.stream()
+                .filter(emergencyCase -> emergencyCase.getSeverityLevel() == severityLevel)
+                .toArray(EmergencyCase[]::new);
+    }
+
     //helper method for the class, to compare nodes by severityLevel
     @Override
     public int compare(EmergencyCase o1, EmergencyCase o2) {
@@ -48,7 +90,7 @@ public class EmergencyWaitlist implements Comparator<EmergencyCase> {
     }
 }
 
-//testing code
+//testing code for BST
 //private EmergencyCase root; //root of the EmergencyWaitlist
 //
 //    /**
